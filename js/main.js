962ffb3e53ff8e5dc0b72fee24fd7fb245b9fd76
unknown <reroach@REROACH04.ins.state.oh.us>
2015-08-18 15:23:02 -0400

    mapping set

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 /// <reference path="typings/jquery/jquery.d.ts"/>
-//$(".panel-body").text("Players:" + players.length);
+var mappedPlayers = players.map(function(p){
+	var pnum = p.num;
+	for(var i=0;i<playerData.length;i++){
+		var pdtl = playerData[i].toLowerCase().split(",");
+		if(pdtl[2]==pnum){
+			var lname = pdtl[3].split(" ");
+			var pname = p.name;
+			if(pname.toLowerCase().indexOf(lname[0])>-1){
+				p["url"] = pdtl[4]; 
+				//p["mp"] = pdtl[3];
+			}
+	    }
+	}  
+	return p;
+});
+
+$(".panel-body").after("<div>Players:" + players.length+ "<br/>"+ playerData.length +"</div>");
+mappedPlayers.map(function(mp){
+	$(".panel-body").after("<div> - player:" + mp.name+ " | "+ mp.url +"</div>");
+});
